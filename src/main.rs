cf0aae871dcb0d88f92c0650d607a96b8bd81aa6
Liam Watts <liam.watts271@gmail.com>
2024-12-08 12:27:37 +1100

    stage2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![allow(unused_imports)]
+
+use std::io::Write;
 use std::net::TcpListener;
+use codecrafters_kafka::api::KafkaResponse;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,7 +11,12 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
+                let response = KafkaResponse::new();
+                let response_bytes: Vec<u8> = response.to_bytes().collect();
+                stream
+                    .write_all(&response_bytes)
+                    .expect("returning a response shouldn't fail");
                 println!("accepted new connection");
             }
             Err(e) => {
